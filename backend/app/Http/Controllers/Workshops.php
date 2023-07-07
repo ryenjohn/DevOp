139e0bf1120f5104633df60bb04bbbe3d2aebf59
SreymomChhum <global-momkpc310@gmail.com>
2023-07-07 10:03:51 +0700

    [US-121] get detail workshop

--- backend/app/Http/Controllers/Workshops.php
+++ backend/app/Http/Controllers/Workshops.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Models\WorkShop;
 
@@ -28,8 +29,6 @@ class Workshops extends Controller
             ->select('work_shops.id','work_shops.name', 'work_shops.img', 'addresses.link', 'schools.name as school_name', 'work_shops.description', 'work_shops.start_date', 'work_shops.expired_date', 'work_shops.time')
             ->where('work_shops.id', $id)
             ->first();
-
-
         return response()->json(['success'=>true,'data'=>$workshops],200);
 
 
