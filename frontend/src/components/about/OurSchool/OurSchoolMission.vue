1ae602a65760d03f5e0dbd31e202b7ef9c5779e5
Seyla Loeub <seylaloeub@gmail.com>
2023-07-11 10:14:58 +0700

    [US-137] cheange mission

--- frontend/src/components/about/OurSchool/OurSchoolMission.vue
+++ frontend/src/components/about/OurSchool/OurSchoolMission.vue
@@ -1,26 +1,41 @@
 <template>
-    <div class="mision">
-        <div class="text">
-            <div class="missiontext">
-                <h4>MISSION</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quia! Tenetur quis rerum iste sint in accusamus 
-                    fugit deleniti mollitia aliquam quas delectus hic modi adipisci dolore natus, molestias architecto.
-                </p>
-            </div>
-        </div>
-        <div class="image">
-            <img src="../../../assets/images/pn1.jpg" alt="" style=" width: 600px ; height: 300px;">
-        </div>
+  <div class="mision">
+    <div class="text">
+      <div class="missiontext">
+        <h4>MISSION</h4>
+        <p>
+          Passerelles numériques is a non-profit organization under French law,
+          created in 2005, operating in three Asian countries: Cambodia, the
+          Philippines, Vietnam and Madagascar. Our mission is to provide
+          education, technical and professional training in the digital sector
+          to young underprivileged people by leveraging their potential and
+          willpower. We endeavour to truly develop their employability which
+          will allow them and their families to escape poverty in a sustainable
+          way, and contribute to the social and economic development of their
+          countries. Our beneficiaries are typically underprivileged or
+          disadvantaged, in extremely precarious situations, according to the
+          criteria PN has defined/specified for each of the countries in which
+          it operates.
+        </p>
+      </div>
     </div>
+    <div class="image">
+      <img
+        src="../../../assets/images/pn1.jpg"
+        alt=""
+        style="width: 600px; height: 300px"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
-    .mision{
-        display: flex;
-        width: 100%;
-    }
-    .text{
-        margin: auto;
-        width: 50%;
-    }
+.mision {
+  display: flex;
+  width: 100%;
+}
+.text {
+  margin: auto;
+  width: 50%;
+}
 </style>
--- frontend/src/components/about/OurSchool/OurSchoolVision.vue
+++ frontend/src/components/about/OurSchool/OurSchoolVision.vue
@@ -1,27 +1,35 @@
 <template>
-    <div class="mision">
-        <div class="text">
-            <div class="missiontext">
-                <h4>VISION</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quia! Tenetur quis rerum iste sint in accusamus 
-                    fugit deleniti mollitia aliquam quas delectus hic modi adipisci dolore natus, molestias architecto.
-                </p>
-            </div>
-        </div>
-        <div class="image">
-            <img src="../../../assets/images/pn2.jpg" alt="" style=" width: 600px ; height: 400px;">
-        </div>
+  <div class="mision">
+    <div class="text">
+      <div class="missiontext">
+        <h4>VALUE</h4>
+        <p>
+          Trust <br />
+          Responsibility <br />
+          Solidarity <br />
+          Respect <br>
+          Demanding approach
+        </p>
+      </div>
     </div>
+    <div class="image">
+      <img
+        src="../../../assets/images/pn2.jpg"
+        alt=""
+        style="width: 600px; height: 400px"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
-    .mision{
-        display: flex;
-        flex-direction: row-reverse;
-        width: 100%;
-    }
-    .text{
-        margin: auto;
-        width: 50%;
-    }
+.mision {
+  display: flex;
+  flex-direction: row-reverse;
+  width: 100%;
+}
+.text {
+  margin: auto;
+  width: 50%;
+}
 </style>
