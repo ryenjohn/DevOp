5b845bbb34db0516671226ac6e16de3b25600eca
makara-12 <makara.phoem@student.passerellesnumeriques.org>
2023-07-17 17:51:36 +0700

    [US-68] Update relationship between workshop and shool

--- backend/app/Http/Controllers/SkillController.php
+++ backend/app/Http/Controllers/SkillController.php
@@ -14,25 +14,6 @@ use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
-
-    // public function getmajors()
-    // {
-    //         $skills = Skill::all();
-    //         $skills = SkillResource::collection($skills);
-    //         return response()->json(['success'=>true,'data'=>$skills],200);
-
-    // }
-   
-    // public function getmajor($id){
-    //     $skill= Skill::find($id);
-    //     if($skill!=''){
-    //         $skill= new ShowSkillResource($skill);
-    //         return response()->json(['success'=>true,'data'=>$skill],200);
-    //     }
-        
-    //     return response()->json(['success'=>true,'data'=>'No data !'],200);
-    // }
-    //
     public function getSkills()
     {
         $skills = Skill::all();
