d9cad6853a4003ec5488bfe696a91f9c1c2f32fb
makara-12 <makara.phoem@student.passerellesnumeriques.org>
2023-07-18 09:21:40 +0700

    [US-50] Update data majors

--- backend/app/Http/Resources/workshopResource.php
+++ backend/app/Http/Resources/workshopResource.php
@@ -15,7 +15,12 @@ class workshopResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            
+            'id' => $this->id,
+            'work_shop' => $this->name,
+            'image' => $this->image,
+            'start_date' => $this->start_date,
+            'expired_date' => $this->expired_date,
+            'time' => $this->time
         ];
     }
 }
